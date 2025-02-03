63b0beaa8914cd82931c523c5790be4c698668ee
ayanmanna123 <mannaayan777@gmail.con>
2025-02-03 11:58:52 +0530

    new

--- src/components/Script1.js
+++ src/components/Script1.js
@@ -7,6 +7,31 @@ export default function Script1(props) {
         let newtext= text.toLocaleUpperCase();
         setText(newtext);
     }
+    const handleloclick =()=>{
+      console.log("was click");
+      let newtext= text.toLocaleLowerCase();
+      setText(newtext);
+  }
+  const handleboldclick =()=>{
+    console.log("was click");
+    setText(<span className="fw-bold">{text}</span>);
+     
+  } 
+    const handlerevclick =()=>{
+      console.log("was click");
+      let newtext = "";
+      for (let i = text.length - 1; i >= 0; i--) {
+        newtext += text[i];
+      }
+      setText(newtext);
+       
+      }
+      const handllnclick =()=>{
+        let newText = text.split(" ").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
+        setText(newText);
+         
+        }
+
     const handelOnChange =(event)=>{
         console.log("on change");
         setText(event.target.value);
@@ -25,8 +50,20 @@ export default function Script1(props) {
           id="exampleFormControlTextarea1"
           rows="8"
         ></textarea>
+      </div> 
+      <div className="containier my-3">
+      <button className="btn btn-primary mx-2" onClick={handleUpclick}>Convert to upper case </button>
+      <button className="btn btn-primary mx-2" onClick={handleloclick}>Convert to lower case </button>
+      <button className="btn btn-primary mx-2" onClick={handleboldclick}>Convert to bold case </button>
+      <button className="btn btn-primary mx-2" onClick={handlerevclick}>Convert to rev case </button>
+      <button className="btn btn-primary mx-2" onClick={handllnclick}>Convert to rev case </button>
+      <h1> Your text summary </h1>
+      <div className="langth">total character {text.length} total word {text.split(" ").length}</div>
+      <p>{0.008 * text.split(" ").length}</p>
+      <h2>preview</h2>
+      <p>{text}</p>
+
       </div>
-      <button className="btn btn-primary" onClick={handleUpclick}>Convert to upper case </button>
     </>
   );
 }
